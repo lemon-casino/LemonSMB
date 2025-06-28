0cf7e28afe24bb888ff500ea43b0e73e3bb58e90
Lemon <1599456917@qq.com>
2025-06-28 17:05:57 +0800

    首次提交

---
+++ src/main/java/org/example/lemonsmb/LemonSmbApplication.java
@@ -0,0 +1,13 @@
+package org.example.lemonsmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonSmbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonSmbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lemonsmb/LemonSmbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lemonsmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonSmbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
